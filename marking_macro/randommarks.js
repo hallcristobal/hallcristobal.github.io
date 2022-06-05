fbbdc6e2b22060ec21a996e299b01c4d791b414e
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2022-06-04 20:48:57 -0400

    randomize no matter what is selected.

--- marking_macro/randommarks.js
+++ marking_macro/randommarks.js
@@ -1,38 +1,22 @@
 (function () {
-    var g_markers = [
-        "attack1",
-        "attack2",
-        "bind1",
-        "bind2",
-        "attack3",
-        "attack4",
-        "ignore1",
-        "ignore2"
-    ];
-
-    var g_targets = [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-    ];
-
     function generateMacro(targets, markers) {
-        var selected = [];
+        var selectedmk = [];
+        var selectedtarg = [];
         var output = [];
-        var minlen = targets.length > markers.length ? markers.length : targets.length;
+        var minlen = Math.min(markers.length, targets.length);
         for (var i = 0; i < minlen; i++) {
-            var r = Math.floor(Math.random() * 100) % minlen;
-            while (selected.indexOf(r) !== -1) {
-                r = Math.floor(Math.random() * 100) % minlen;
+            var r = Math.floor(Math.random() * 100) % markers.length;
+            while (selectedmk.indexOf(r) !== -1) {
+                r = Math.floor(Math.random() * 100) % markers.length;
+            }
+            var m = Math.floor(Math.random() * 100) % targets.length;
+            while (selectedtarg.indexOf(m) !== -1) {
+                m = Math.floor(Math.random() * 100) % targets.length;
             }
 
-            selected.push(r);
-            var s = "/mk " + markers[r] + " <" + targets[i] + ">";
+            selectedmk.push(r);
+            selectedtarg.push(m);
+            var s = "/mk " + markers[r] + " <" + targets[m] + ">";
             output.push(s);
         }
         var ret = output.join("\n");
@@ -41,7 +25,29 @@
 
     if (typeof module !== "undefined") {
         if (module.exports) {
-            console.log(generateMacro(g_targets, g_markers)); // nodejs
+            var markers = [
+                "attack1",
+                "attack2",
+                "bind1",
+                "bind2",
+                "attack3",
+                "attack4",
+                "ignore1",
+                "ignore2"
+            ];
+
+            var targets = [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+            ];
+
+            console.log(generateMacro(targets, markers)); // nodejs
         }
     } else {
         window.Generate = {
